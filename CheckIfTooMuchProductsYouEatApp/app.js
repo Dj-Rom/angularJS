3b84947daa0855dcc5cf702ec140e1742ae638a2
Aleh Kapusta <118632628+Dj-Rom@users.noreply.github.com>
2024-09-18 17:55:47 +0200

    Add files via upload

---
+++ CheckIfTooMuchProductsYouEatApp/app.js
@@ -0,0 +1,30 @@
+(function () {
+  "use strict";
+  angular
+    .module('ProductsApp', [])
+    .controller('ProductsController', ProductsController);
+
+  ProductsController.$inject = ['$scope', '$filter'];
+
+  function ProductsController($scope) {
+    $scope.products = "";
+    $scope.answerForUser = ""
+    $scope.howMuchEatForLunch = function () {
+      console.log($scope.products.length);
+
+      if ($scope.products.length === 0) {
+        $scope.answerForUser = "Please enter data first!"
+      } else {
+        var resultArray = $scope.products.split(","); //create a list of products
+        var filteredArray = resultArray.map(e => e.trim()).filter(e => e !== ''); // remove empty strings
+        if (filteredArray.length === 0) {
+          $scope.answerForUser = "Please enter data first from words!"
+        } else {
+          $scope.answerForUser = filteredArray.length <= 3 ? "Enjoy!" : "Too much!";
+        }
+
+      }
+    };
+
+  }
+})();
